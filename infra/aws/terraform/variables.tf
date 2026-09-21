a48c6878cfc6725901793f74ce272bc876e678eb
sokldjs554 <97650034+sokldjs554@users.noreply.github.com>
2026-09-21 15:52:34 +0900

    style: format Terraform inputs and outputs

--- infra/aws/terraform/variables.tf
+++ infra/aws/terraform/variables.tf
@@ -1,5 +1,22 @@
-variable "aws_region" { type = string, default = "ap-northeast-2" }
-variable "project" { type = string, default = "pickup-pact" }
-variable "vpc_id" { type = string }
-variable "private_subnet_ids" { type = list(string) }
-variable "db_password" { type = string, sensitive = true }
+variable "aws_region" {
+  type    = string
+  default = "ap-northeast-2"
+}
+
+variable "project" {
+  type    = string
+  default = "pickup-pact"
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "private_subnet_ids" {
+  type = list(string)
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
