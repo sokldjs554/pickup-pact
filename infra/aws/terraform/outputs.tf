925620b3c11f46d523854282cd2f24867d2d89b3
sokldjs554 <97650034+sokldjs554@users.noreply.github.com>
2026-09-21 15:52:36 +0900

    style: format Terraform inputs and outputs

--- infra/aws/terraform/outputs.tf
+++ infra/aws/terraform/outputs.tf
@@ -1,3 +1,11 @@
-output "postgres_endpoint" { value = aws_db_instance.postgres.address }
-output "redis_endpoint" { value = aws_elasticache_replication_group.redis.primary_endpoint_address }
-output "msk_arn" { value = aws_msk_serverless_cluster.events.arn }
+output "postgres_endpoint" {
+  value = aws_db_instance.postgres.address
+}
+
+output "redis_endpoint" {
+  value = aws_elasticache_replication_group.redis.primary_endpoint_address
+}
+
+output "msk_arn" {
+  value = aws_msk_serverless_cluster.events.arn
+}
