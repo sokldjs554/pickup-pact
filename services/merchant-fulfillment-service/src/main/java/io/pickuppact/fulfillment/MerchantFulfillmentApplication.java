ce01c932ad7d44ed8e79eff6ca4ffe7e8880a7ec
sokldjs554 <97650034+sokldjs554@users.noreply.github.com>
2026-09-23 00:49:39 +0900

    feat: inject clock into merchant fulfillment domain

--- services/merchant-fulfillment-service/src/main/java/io/pickuppact/fulfillment/MerchantFulfillmentApplication.java
+++ services/merchant-fulfillment-service/src/main/java/io/pickuppact/fulfillment/MerchantFulfillmentApplication.java
@@ -1,7 +1,9 @@
 package io.pickuppact.fulfillment;
 
+import java.time.Clock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -9,5 +11,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class MerchantFulfillmentApplication {
     public static void main(String[] args) {
         SpringApplication.run(MerchantFulfillmentApplication.class, args);
+    }
+
+    @Bean
+    Clock fulfillmentClock() {
+        return Clock.systemUTC();
     }
 }
